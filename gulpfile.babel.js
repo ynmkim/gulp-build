844aeef3fb0da99e7df665e64df7c8bef189e70f
김윤미 <younmeet@gmail.com>
2023-02-28 16:11:16 +0900

    gulp-pug 설치

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,17 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";
 
-export const dev = () => console.log("I will dev");
+const routes = {
+  pug: {
+    src: "src/**/*.pug",
+    dest: "build"
+  }
+};
+
+export const pug = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(gpug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
