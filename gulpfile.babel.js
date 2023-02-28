6714484d7dadf6829f8e2a67c8f89413b7cc8469
김윤미 <younmeet@gmail.com>
2023-02-28 16:38:14 +0900

    gulp-webserver 설치

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
+import ws from "gulp-webserver";
 
 const routes = {
   pug: {
@@ -15,10 +16,14 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
+
+const webserver = () => gulp.src("build").pipe(ws({livereloard: true, open: true}));
 
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver])
+
+export const dev = gulp.series([prepare, assets, postDev]);
