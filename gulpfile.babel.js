d2b9f131f7b43a4ef4f0769dd4c101acbcc56094
김윤미 <younmeet@gmail.com>
2023-02-28 17:38:19 +0900

    gulp-image 설치

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,17 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import image from "gulp-image";
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
   }
 };
 
@@ -25,7 +30,13 @@ const watch = () => {
   gulp.watch(routes.pug.watch, pug);
 }; 
 
-const prepare = gulp.series([clean]);
+const img = () =>
+  gulp
+  .src(routes.img.src)
+  .pipe(image())
+  .pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
