46e0ba7416c37fffe33b67a392f8ff30241ca92d
김윤미 <younmeet@gmail.com>
2023-03-01 15:45:18 +0900

    gulp-bro, uglifyify, bablify 설치

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ import image from "gulp-image";
 const sass = require("gulp-sass")(require("node-sass"));
 import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
+import bro from "gulp-bro";
+import babelify from "babelify";
 
 sass.compiler = require("node-sass");
 
@@ -24,6 +26,11 @@ const routes = {
     watch: "src/**/*.scss",
     src: "src/scss/style.scss",
     dest: "build/css"
+  },
+  js: {
+    watch: "src/js/**/*.js",
+    src: "src/js/main.js",
+    dest: "build/js"
   }
 };
 
@@ -40,6 +47,7 @@ const webserver = () => gulp.src("build").pipe(ws({ livereloard: true, open: tru
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.scss.watch, styles);
+  gulp.watch(routes.js.watch, js);
 }; 
 
 const img = () =>
@@ -61,9 +69,22 @@ const styles = () =>
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
 
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }]
+        ]
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
+
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([pug, img, styles]);
+const assets = gulp.series([pug, img, styles, js]);
 
 const postDev = gulp.parallel([webserver, watch]);
 
